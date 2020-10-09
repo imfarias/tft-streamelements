81f2cf08a9371da3afbe14896d819cf248855517
root <root@DESKTOP-FDAQQRO.localdomain>
2020-10-09 16:14:31 -0300

    Fix port node server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 const http = require('http');
 const url = require('url');
 
-http.createServer(onRequest).listen(8000);
+http.createServer(onRequest).listen(80);
 
 function onRequest(request, response) {
 
