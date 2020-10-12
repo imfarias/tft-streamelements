4367abf5fbea130a1a34491da10a51d62aeabfe6
root <root@DESKTOP-FDAQQRO.localdomain>
2020-10-12 15:50:52 -0300

    Fix UTF-8 Ranking

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ function onRequest(request, response) {
     
         ranking += ` - ${data.player_info.leaguePoints} PDL`;
 
-        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
         response.write(ranking);
         response.end();
     })();
